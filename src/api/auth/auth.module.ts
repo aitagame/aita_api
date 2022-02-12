b7c28a18b860fd8b164255a39fc4f0d0647644f8
cybor97 <muhametshin.1997@gmail.com>
2022-02-12 11:46:09 +0100

    AITA-1: Add socket.io

--- src/api/auth/auth.module.ts
+++ src/api/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { AuthEventsGateway } from './auth.gateway';
 import { AuthService } from './auth.service';
 
 @Module({
   imports: [],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthEventsGateway],
 })
 export class AuthModule { }

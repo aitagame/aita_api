dacf3f4ecccb5c2e9f6db18fbc535938904bf8fc
vitalij volkov <vitalij.volkov@itp-it.com>
2022-02-17 15:10:44 +0300

    AITA-14 Check .env and add package-lock file

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  if(!process.env['JWT_SECRET'])
-    throw new Error('Shit happened, cannot find jwt secret in environment');
-    
+  if(!process.env['JWT_SECRET'] || !process.env['PASSWORD_HASH_SALT'])
+    throw new Error('Shit happened, cannot find jwt secret or salt in environment');
+
   await app.listen(process.env['AITA_API_PORT'], 'localhost');
 }
 bootstrap();

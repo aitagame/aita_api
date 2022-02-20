37c16b91d93faaa15788b0d35fb81356e0a8ce31
cybor97 <muhametshin.1997@gmail.com>
2022-02-20 16:23:02 +0100

    AITA-17: Implement redis service

--- src/api/rooms/rooms.module.ts
+++ src/api/rooms/rooms.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import RedisModule from "src/storage/redis/redis.module";
 import { User } from "../users/user.model";
 import { UserModule } from "../users/user.module";
 import { RoomsEventsGateway } from "./rooms.gateway";
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forFeature([User])],
+  imports: [UserModule, TypeOrmModule.forFeature([User]), RedisModule],
   providers: [RoomsEventsGateway]
 })
 export class RoomsModule { };
--- src/storage/redis/redis.service.ts
+++ src/storage/redis/redis.service.ts
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { createClient, RedisClientType, RedisModules } from "redis";
 
 export class RedisService {
@@ -5,7 +6,72 @@ export class RedisService {
     this.redisClient = createClient();
   }
 
-  async get(key): Promise<string> {
+  async onModuleInit(): Promise<void> {
+    await this.redisClient.connect();
+  }  
+
+  async keys(wildcard: string): Promise<string[]> {
+    return await this.redisClient.keys(wildcard);
+  }
+
+  async get(key: string): Promise<string> {
     return await this.redisClient.get(key);
+  }
+
+  async incr(key: string, by: number = 1): Promise<number> {
+    if (by > 1) {
+      return await this.redisClient.incrBy(key, by);
+    }
+    return await this.redisClient.incr(key);
+  }
+
+  async set(key: string, value: string): Promise<boolean> {
+    return await this.redisClient.set(key, value) === 'OK';
+  }
+
+  async del(key: string): Promise<boolean> {
+    return !isNaN(await this.redisClient.del(key));
+  }
+
+  async hKeys(key: string): Promise<string[]> {
+    return await this.redisClient.hKeys(key);
+  }
+
+  async hLen(key: string): Promise<number> {
+    return await this.redisClient.hLen(key);
+  }
+
+  async hGet(key: string, field: string): Promise<string> {
+    return await this.redisClient.hGet(key, field);
+  }
+
+  async hDel(key: string, field: string): Promise<boolean> {
+    return !isNaN(await this.redisClient.hDel(key, field));
+  }
+
+  async hSet(key: string, field: string, value: string): Promise<boolean> {
+    return !isNaN(await this.redisClient.hSet(key, field, value));
+  }
+
+  async hmGet(key: string, fields: string[] | object): Promise<object> {
+    assert(fields !== null, new Error('Data cannot be null'));
+    const keys = fields instanceof Array ? fields : Object.keys(fields);
+    const data = await this.redisClient.hmGet(key, keys);
+
+    return keys.reduce((prev, value, index) => {
+      prev[value] = data[index];
+      return prev
+    }, {});
+  }
+
+  async hmSet(key: string, data: object | Map<string, string>): Promise<boolean> {
+    assert(data !== null, new Error('Data cannot be null'));
+
+    const resp = await this.redisClient.hSet(key, data instanceof Map
+      ? data
+      : Object.keys(data).reduce((map, key) => map.set(key, data[key]), new Map<string, string>())
+    );
+
+    return !isNaN(resp);
   }
 }
